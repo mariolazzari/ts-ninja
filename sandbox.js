f43a26da94d3f0fc48a12cebd58d8703954b4a85
Mario Lazzari <mario.lazzari@gmail.com>
2020-09-19 21:37:31 +0200

    Done lesson 4

--- sandbox.js
+++ sandbox.js
@@ -14,3 +14,46 @@ var circ = function (diamenter) {
 };
 //circ("ciao")
 console.log(circ(10).toFixed(2));
+// Arrays
+var names = ["Mairo", "Mariarosa", "Maria"];
+names.push("Roby");
+//+names.push(4);
+console.log(names);
+var numbers = [10, 20, 30, 40];
+numbers.push(50);
+//numbers.push("ciao");
+console.log(numbers);
+var mixed = ["Mario", 45, "Mariarosa", 44];
+mixed.push("Maria");
+mixed.push(70);
+mixed[0] = 45;
+mixed[1] = "Mario";
+console.log(mixed);
+// Objects
+var ninja = { name: "Mario", belt: "black", age: 45 };
+ninja.age = 30;
+//ninja.age="30"
+//ninja.skils = ["test"];
+// explicit types
+var character2;
+var age2;
+var loggedIn;
+console.log(character2, age2, loggedIn);
+// typed arrays
+var ninjas = [];
+ninjas.push("Mario");
+// unioon types
+var mixed2 = [];
+mixed2.push("ciao");
+mixed2.push(20);
+console.log(mixed2);
+var uid;
+uid = 123;
+uid = "123";
+// objects
+var ninja1;
+ninja1 = { name: "Yoshi", age: 30 };
+var ninja2;
+ninja2.age = 44;
+ninja2.name = "Mario";
+console.log(ninja1, ninja2);
--- sandbox.ts
+++ sandbox.ts
@@ -19,3 +19,57 @@ const circ = (diamenter: number) => {
 
 //circ("ciao")
 console.log(circ(10).toFixed(2));
+
+// Arrays
+let names = ["Mairo", "Mariarosa", "Maria"];
+names.push("Roby");
+//+names.push(4);
+console.log(names);
+
+let numbers = [10, 20, 30, 40];
+numbers.push(50);
+//numbers.push("ciao");
+console.log(numbers);
+
+let mixed = ["Mario", 45, "Mariarosa", 44];
+mixed.push("Maria");
+mixed.push(70);
+mixed[0] = 45;
+mixed[1] = "Mario";
+console.log(mixed);
+
+// Objects
+let ninja = { name: "Mario", belt: "black", age: 45 };
+ninja.age = 30;
+//ninja.age="30"
+//ninja.skils = ["test"];
+
+// explicit types
+let character2: string;
+let age2: number;
+let loggedIn: boolean;
+console.log(character2, age2, loggedIn);
+
+// typed arrays
+let ninjas: string[] = [];
+ninjas.push("Mario");
+
+// unioon types
+let mixed2: (string | number)[] = [];
+mixed2.push("ciao");
+mixed2.push(20);
+console.log(mixed2);
+
+let uid: string | number;
+uid = 123;
+uid = "123";
+
+// objects
+
+let ninja1: object;
+ninja1 = { name: "Yoshi", age: 30 };
+
+let ninja2: { name: string; age: number };
+ninja2.age = 44;
+ninja2.name = "Mario";
+console.log(ninja1, ninja2);
